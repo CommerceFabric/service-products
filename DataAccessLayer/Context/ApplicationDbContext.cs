c0ba4893e8dfe7aaa4c25cb18aac8e3838b769c5
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-06-22 09:39:00 +0100

    feat: add MySQL dbContext, product entity, and injection

---
+++ DataAccessLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region DB Tables
+        public DbSet<Product> Products { get; set; }
+        #endregion
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
