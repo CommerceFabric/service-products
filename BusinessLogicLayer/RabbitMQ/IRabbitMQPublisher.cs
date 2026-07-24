829d1b6b6a0577ff0c40530a39835ae876078df6
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-07-24 11:06:50 +0100

    feat: add rabbitMQ to project and created publisher skeleton with lazy loading of async connection to the channel

---
+++ BusinessLogicLayer/RabbitMQ/IRabbitMQPublisher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.RabbitMQ
+{
+    public interface IRabbitMQPublisher
+    {
+        Task Publish<T>(string routingKey, T message);
+    }
+}
---
+++ BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.RabbitMQ
+{
+    public class RabbitMQPublisher : IRabbitMQPublisher, IDisposable
+    {
+        private readonly IConfiguration _configuration;
+        private IChannel _channel;
+        private IConnection _connection;
+        private readonly SemaphoreSlim _lock = new(1, 1); // Semaphore to ensure thread safety when creating the channel (has 1 permit, so only one thread can enter at a time)
+
+        public RabbitMQPublisher(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Dispose()
+        {
+            _channel.Dispose();
+            _connection.Dispose();
+        }
+
+        public async Task Publish<T>(string routingKey, T message)
+        {
+            await EnsureConnectedAsync(); // Ensure that the channel is created and connected to RabbitMQ before publishing the message (has been lazy loaded)
+            // todo - implement the actual publishing logic here, using the _channel to publish the message to RabbitMQ
+        }
+
+        /// <summary>
+        /// Lazy initialization of the RabbitMQ channel. If the channel is already created, it returns immediately. Otherwise, it creates a new connection and channel to RabbitMQ.
+        /// Required as it needs async methods to create the connection and channel, and we want to avoid creating them in the constructor.
+        /// So instead, we create them on demand when the first message is published.
+        /// Afterwhich, the channel is reused for subsequent messages.
+        /// </summary>
+        /// <returns></returns>
+        private async Task EnsureConnectedAsync()
+        {
+            if (_channel != null) // if the channel is already created, return immediately (we have already lazy initialized the connection and channel for rabbitMQ)
+                return;
+
+            await _lock.WaitAsync(); // Use a semaphore to ensure that only one thread can create the connection and channel at a time (so that we don't create multiple connections and channels if multiple threads call Publish at the same time prior to the channel being initialized)
+
+
+            try
+            {
+                if (_channel != null) // sanity check to see if the channel was created while waiting for the lock, if so, return immediately
+                    return;
+
+                // Create a new connection and channel to RabbitMQ
+                var factory = new ConnectionFactory
+                {
+                    HostName = _configuration["RABBITMQ_HOST"]!,
+                    UserName = _configuration["RABBITMQ_USER"]!,
+                    Password = _configuration["RABBITMQ_PASSWORD"]!,
+                    Port = int.Parse(_configuration["RABBITMQ_PORT"]!)
+                };
+                _connection = await factory.CreateConnectionAsync();
+                _channel = await _connection.CreateChannelAsync();
+            }
+            finally
+            {
+                _lock.Release(); // Release the semaphore so that other threads can enter and use the channel
+            }
+        }
+    }
+}
